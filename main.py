eb263dc143da20e5879b01760dd9c82a4d8e7053
lee <lee@chris-macbook.local>
2024-05-22 12:03:01 +0800

    20240522_2

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host="0.0.0.0", port=8080)
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
+    # app.run(debug=True, port=8080)
